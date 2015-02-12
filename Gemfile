4c50399c32045f2f8f164e43f90c5cfcd0bf5630
Max Trubetskoy <handcrab@gmail.com>
2015-02-13 00:39:54 +0300

    add votes to links

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'devise'
 gem 'bootstrap-sass', '~> 3.3.3'
 gem "haml-rails", "~> 0.8"
 
+gem 'acts_as_votable', '~> 0.10.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,5 @@
 class LinksController < ApplicationController
-  before_action :set_link, only: [:show, :edit, :update, :destroy]
+  before_action :set_link, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
   before_action :authenticate_user!, :except => [:index, :show]
 
 
@@ -62,6 +62,18 @@ class LinksController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+
+  def upvote
+    @link.upvote_by current_user
+    redirect_to :back
+  end
+
+  def downvote
+    @link.downvote_by current_user
+    redirect_to :back
+  end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
