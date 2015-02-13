07712178496e1547a998800a7ef0d7af53fb2e79
Max Trubetskoy <handcrab@gmail.com>
2015-02-13 05:14:12 +0300

    set the default user for links without author

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,19 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :links       
+  has_many :links  
+
+  before_destroy :adopt_orphant_links
+
+  
+  # TODO
+  def self.default_user
+    find_by(email: 'default@default.default') || create(email: 'default@default.default', password: 'defaultdefault', name: 'default')
+  end
+
+  private
+    def adopt_orphant_links
+      self.links.update_all user_id: User.default_user
+      # self.links.each {|link| link.update_attributes user_id: User.default_user}
+    end     
 end
